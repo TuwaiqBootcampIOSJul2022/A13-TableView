18101a0016c529bcf025e0efb92bbebb5d7625ad
Ruba <ruba@Rubas-MacBook-Pro.local>
2022-08-28 00:36:44 +0300

    commit

---
+++ Books/Books/tableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  tableViewCell.swift
+//  Books
+//
+//  Created by Ruba on 27/01/1444 AH.
+//
+
+import UIKit
+
+class tableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+ 
+}
+
